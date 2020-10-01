5c52e103d9986f6ec1a8996b013494324374270e
Evolutionlab <info@evolutionlab.it>
2020-10-01 18:54:33 +0200

    create repro for shell navigation issue

---
+++ ShellTest/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using ShellTest.ViewModels;
+using ShellTest.Views;
+using Xamarin.Forms;
+
+namespace ShellTest
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+		}
+
+	}
+}
---
+++ ShellTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ShellTest.ViewModels;
+
+namespace ShellTest.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			var navStack = ((ShellGroupItem) Parent).Navigation.NavigationStack;
+			var output   = "";
+
+			for (int i = 0; i < navStack.Count; i++)
+			{
+				output += $"Index {i}, page: {navStack[i]?.ToString() ?? "null"} {System.Environment.NewLine}";
+			}
+
+			OutputLabel.Text = output;
+
+		}
+	}
+}
